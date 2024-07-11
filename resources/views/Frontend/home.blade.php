c8a38cf8b97a893da1fe6af004e9fda72118d94d
boula1997 <boula.nessim.soliman@gmail.com>
2024-07-11 12:46:28 +0300

    fixing code to make it work on server

--- resources/views/Frontend/home.blade.php
+++ resources/views/Frontend/home.blade.php
@@ -19,10 +19,10 @@
   <script>
     //paste this code under head tag or in a seperate js file.
     // Wait for window load
-    $(window).load(function() {
-      // Animate loader off screen
-      $(".se-pre-con").fadeOut("slow");;
-    });
+    // $(window).load(function() {
+    //   // Animate loader off screen
+    //   $(".se-pre-con").fadeOut("slow");;
+    // });
   </script>   
 
     <!-- Start of e-learninig Zendesk Widget script -->
